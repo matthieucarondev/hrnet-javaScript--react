5f93dcfbe9e7f0f23d4cbacb77d38bc15fe2e4ee
Matthieu Caron <104826273+matthieucarondev@users.noreply.github.com>
2024-07-16 21:49:23 +0200

    component Root.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,15 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Form from "./pages/Form";
 import EmployeeList from './pages/EmployeeList';
-import Header from './components/Header';
+import Root from './components/Root';
+
  
 function App() {
     return (
-      <Router>
-       <div className="App">
-       <Header/>
-        <Routes>
-       < Route index element={<Form />} />
-       <Route path='/employee-list' element={<EmployeeList />} />
-        </Routes>
-       </div>
-      </Router>
-      
-     
-  
+      <Route path="/" element={<Root />}>
+        < Route index element={<Form />} />
+        <Route path='/employee-list' element={<EmployeeList />} />
+       </Route>
   );
 }
   export default App;
