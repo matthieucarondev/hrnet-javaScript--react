abaacaab06079a1d81d75eb3d3fb97e6549b3a28
Matthieu Caron <104826273+matthieucarondev@users.noreply.github.com>
2024-07-16 23:02:57 +0200

    component Root.jsx modif App 14

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Form from "./pages/Form";
 import EmployeeList from "./pages/EmployeeList";
-import Header from "./components/Header";
+
+import Root from "./components/Root";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Header />
-        
-          <Route index element={<Form />} />
-          <Route path="employee-list" element={<EmployeeList />} />
-        
-      </div>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Root />}>
+        <Route index element={<Form />} />
+        <Route path="employee-list" element={<EmployeeList />} />
+      </Route>
+    </Routes>
+  </Router>
   );
 }
 export default App;
